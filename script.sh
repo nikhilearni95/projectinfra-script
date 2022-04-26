b8ae8c92364da5240eac4bd7f8df58a894f5d921
nikhilearni95 <98696104+nikhilearni95@users.noreply.github.com>
2022-04-26 18:34:14 +0530

    all applications added

--- script.sh
+++ script.sh
@@ -12,6 +12,24 @@ sudo apt-get update
 sudo apt-get install -y jenkins
 sudo systemctl start jenkins
 
-#maven unzip
+#maven unzip installation
 
 sudo apt install -y maven unzip
+
+#sonarqube installation
+
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-6.7.7.zip
+unzip sonarqube-6.7.7.zip
+~/sonarqube-6.7.7/bin/linux-x86-64/sonar.sh start
+
+# Nexus installation
+
+wget https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.29.2-02-unix.tar.gz
+tar xvf nexus-3.29.2-02-unix.tar.gz
+~/nexus-3.29.2-02/bin/nexus start
+
+# Tomcat installtion
+
+wget https://archive.apache.org/dist/tomcat/tomcat-7/v7.0.94/bin/apache-tomcat-7.0.94.tar.gz
+tar xvf apache-tomcat-7.0.94.tar.gz
+
